22c26011c28ac187f71e2383189bf1d75578a8a4
Tobias Bengfort <tobias.bengfort@posteo.de>
2021-05-08 10:42:11 +0200

    use low level APIs
    -	own mainloop, not just render on key
    -	blitting colored strings is complicated

--- tighogg.py
+++ tighogg.py
@@ -1,3 +1,7 @@
+import sys
+import shutil
+import time
+
 import boon
 
 LEFT = 0
@@ -10,8 +14,8 @@ class Player:
     def __init__(self, x, y, direction, color):
         self.x = x
         self.y = y
+        self.direction = direction
         self.color = color
-        self.direction = LEFT
         self.running = False
         self.weapon = '-'
 
@@ -33,26 +37,30 @@ class Player:
             yield r' Λ '
 
     def render(self):
-        for line in self._render():
-            line = line.replace('w', self.weapon)
-            line = (
+        for i, line in enumerate(self._render()):
+            x = self.x - 1 + len(line) - len(line.lstrip())
+            y = self.y - 2 + i
+            boon.move(y, x)
+            sys.stdout.write(
                 boon.get_cap('setaf', self.color)
                 + boon.get_cap('bold')
-                + line
+                + line.strip().replace('w', self.weapon)
                 + boon.get_cap('sgr0')
             )
-            yield line
 
 
-class Game(boon.App):
+class Game:
     def __init__(self):
-        super().__init__()
         self.player1 = Player(10, 10, RIGHT, YELLOW)
-        self.player2 = Player(10, 10, LEFT, GREEN)
+        self.player2 = Player(20, 10, LEFT, GREEN)
+        self.running = True
 
-    def render(self, rows, cols):
-        yield from self.player1.render()
-        yield from self.player2.render()
+    def render(self):
+        cols, rows = shutil.get_terminal_size()
+        sys.stdout.write(boon.get_cap('clear'))
+        self.player1.render()
+        self.player2.render()
+        sys.stdout.flush()
 
     def on_key(self, key):
         if key == 'q':
@@ -77,6 +85,15 @@ class Game(boon.App):
             self.player2.direction = RIGHT
         elif key == 's':
             self.player2.running = False
+
+    def run(self):
+        self.running = True
+        with boon.fullscreen():
+            while self.running:
+                last = time.time()
+                self.on_key(boon.getch())
+                self.render()
+                time.sleep(1 / 30 - (time.time() - last))
 
 
 if __name__ == '__main__':
